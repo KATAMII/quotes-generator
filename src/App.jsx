59a9ace583812b03154d2223f83417648627db6b
KATAMII <lydiamwaah01@gmail.com>
2024-06-04 22:22:41 +0300

    initialize the project and design the UI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Random from './component/Random'
+
+function App() {
+  return (
+    <>
+     <div>
+      <Random/>
+     </div>
+    </>
+  )
+}
+
+export default App
