e5d24771ddba01b264e50ef29a6015098f7b20c1
KATAMII <lydiamwaah01@gmail.com>
2024-06-05 18:53:20 +0300

    correct the code

--- src/component/Random.jsx
+++ src/component/Random.jsx
@@ -1,45 +1,43 @@
-import React from 'react'
-import './../component/Random.css'
-import { useState } from 'react'
-import { AiFillTikTok } from "react-icons/ai";
+import React, { useState, useEffect } from 'react';
+import './../component/Random.css';
 import { FaXTwitter } from "react-icons/fa6";
 import { IoReloadCircle } from "react-icons/io5";
 
 function Random() {
+    const [quote, setQuote] = useState( "Life is a question and how we live it is our answer.");
 
-    let quotes=[]
+  
+    const fetchQuote = async () => {
+        try {
+            const response = await fetch("https://api.adviceslip.com/advice");
+            const data = await response.json();
+           setQuote(data.slip.advice)
+        } catch (error) {
+            console.error("Error fetching the quote:", error);
+            setQuote("Failed to load advice")
+        }
+    };
 
-    async function loadQuotes(){
-        const response = await fetch("https://zenquotes.io/api/random");
-        quotes = await response.json();
-    }
+   
+    useEffect(() => {
+        fetchQuote();
+    }, []);
 
-    const [quote,setQuote] =useState({text:"Life is a question and how we live it is our answer.",
-    author:"Gary Keller",});
-
-    const random = () => {
-        const select =quotes[Math.floor(Math.random()*quotes.length)];
-        setQuote(select);
-    }
-
-    loadQuotes();
-
-  return (
-    <div className='quote-section'>
-        <div className="quotetxt">{quote.text}</div>
-        <div>
-            <div className="line"></div>
-            <div className="below">
-                <div className="author">{quote.author.split(',')[0]}</div>
-                <div className="icons">
-                <IoReloadCircle onClick={()=>{random()}} />
-                <FaXTwitter/>
+    return (
+        <div className='quote-section'>
+            <div className="quotetxt">{quote}</div>
+            <div>
+                <div className="line"></div>
+                <div className="below">
+                    
+                    <div className="icons">
+                        <IoReloadCircle onClick={fetchQuote} />
+                        <FaXTwitter />
+                    </div>
                 </div>
             </div>
         </div>
-      
-    </div>
-  )
+    );
 }
 
-export default Random
+export default Random;
